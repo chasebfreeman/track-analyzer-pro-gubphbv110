9749b61cb14b3f9284f77289ac0c2dcfde6b2a22
chasebfreeman <chase@elitemotorsportsllc.com>
2026-02-08 14:17:55 -0600

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,16 @@ import { ExpoConfig } from 'expo/config';
 const config: ExpoConfig = {
   name: 'Track Analyzer',
   slug: 'track-analyzer',
+  plugins: [
+    [
+      'expo-image-picker',
+      {
+        photosPermission: 'Allow Track Analyzer to access your photos to attach lane pictures.',
+        cameraPermission: 'Allow Track Analyzer to use your camera to take lane pictures.',
+      },
+    ],
+  ],
 };
 
 export default config;
+
